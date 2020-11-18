bd3d7227d880f58efb941c4e5e95007cd7b2087b
foeu <fabiano.junior@gft.com>
2020-11-18 17:08:45 -0300

    Initial commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace estudo_api.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base (options)
+        {
+        }
+    }
+}
